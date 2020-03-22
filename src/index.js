f5e9c4adf842926140df0232e210cdbcf40824e6
gingerfox1998 <60549675+gingerfox1998@users.noreply.github.com>
2020-03-23 02:02:38 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@
 
 module.exports = function towelSort (matrix) {
   var newarr = [];
+  var res = [];
   for(i = 0; i < matrix.length; i++) {
     for(j = 0; j < matrix[i].length; j++) {
-        newarr.push(matrix[i][j]);
+        newarr = newarr.push(matrix[i][j]);
     }
   }
   newarr.sort(function(a, b) {
