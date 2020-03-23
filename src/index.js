06293721a4ae23aee3aec578aa300e48a3208d8d
gingerfox1998 <60549675+gingerfox1998@users.noreply.github.com>
2020-03-24 00:44:43 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ module.exports = function towelSort (matrix) {
   var result = [];
   for(i = 1; i < matrix.length; i++) {
     if(i % 2 == 0)
-        result = result.concat(matrix[i - 1]);
-    else
         result = result.concat(matrix[i - 1].reverse());
+    else
+        result = result.concat(matrix[i - 1]);
   }
   return result;
 }
